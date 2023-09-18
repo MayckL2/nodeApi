c1ad9075c8d37b1cb32a614dcb38857fbc8cf441
Mayck <103364944+MayckL2@users.noreply.github.com>
2023-09-18 00:56:22 -0300

    Add files via upload

--- back/app.js
+++ back/app.js
@@ -5,6 +5,7 @@ const { stringify } = require('querystring')
 
 app.use(express.json())
 
+// CORS
 app.use((req, res, next) => {
     console.log('Cors funcionando!')
     res.header('Access-Control-Allow-Origin', '*')
@@ -12,11 +13,42 @@ app.use((req, res, next) => {
     next()
 })
 
-app.get('/prod', (req, res, next) => {
-    // res.json({ msg: 'Retorno da api!' })
-    res.json({status: 'sucesso', msg: 'Retorno da api!'})
+// REQUISIÇÃO GET
+app.get('/prod', (req, res) => {
+    res.json({status: 'sucesso', msg: 'Retorno tipo GET!'})
 })
 
+// REQUISIÇÃO POST
+app.post('/prod', (req, res) => {
+    if(req){
+        res.json({status: 'sucesso', msg: 'Retorno tipo POST!'})
+        console.log(req.body)
+    }else{
+        res.json({status: 'false', msg: 'Erro na requisição!'})
+    }
+})
+
+// REQUISIÇÃO PUT
+app.put('/prod', (req, res) => {
+    if(req){
+        res.json({status: 'sucesso', msg: 'Retorno tipo PUT!'})
+        console.log(req.body)
+    }else{
+        res.json({status: 'false', msg: 'Erro na requisição!'})
+    }
+})
+
+// REQUISIÇÃO DELETE
+app.delete('/prod', (req, res) => {
+    if(req){
+        res.json({status: 'sucesso', msg: 'Retorno tipo DELETE!'})
+        console.log(req.body)
+    }else{
+        res.json({status: 'false', msg: 'Erro na requisição!'})
+    }
+})
+
+// RETORNO DA ROTA PRINCIPAL
 app.get('/', (req, res) => {
     res.json({msg: 'Hello World'})
 })
--- front/scritp.js
+++ front/scritp.js
@@ -1,9 +1,64 @@
-window.addEventListener('load', async ()=>{
-    let json
-    console.log('hola')
-    const res = await fetch('http://localhost:3000/prod')
+let retorno = document.querySelector('#retorno')
+
+// REQUISIÇÃO TIPO GET
+document.querySelector('#get').addEventListener('click', async () => {
+
+    const res = await fetch('http://localhost:3000/prod', {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
     const dados = await res.json()
-    
-    document.querySelector('h1').innerHTML = dados.msg
+
     console.log(dados)
+    retorno.innerHTML = dados.msg
+})
+
+// REQUISIÇÃO TIPO POST
+document.querySelector('#post').addEventListener('click', async () => {
+
+    const res = await fetch('http://localhost:3000/prod', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({status: 'sucess', msg: 'postagem feita!'})
+    })
+    const dados = await res.json()
+
+    console.log(dados)
+    retorno.innerHTML = dados.msg
+})
+
+// REQUISIÇÃO TIPO PUT
+document.querySelector('#put').addEventListener('click', async () => {
+
+    const res = await fetch('http://localhost:3000/prod', {
+        method: 'PUT',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({status: 'sucess', msg: 'putagem feita! kkk'})
+    })
+    const dados = await res.json()
+
+    console.log(dados)
+    retorno.innerHTML = dados.msg
+})
+
+// REQUISIÇÃO TIPO DELETE
+document.querySelector('#delete').addEventListener('click', async () => {
+
+    const res = await fetch('http://localhost:3000/prod', {
+        method: 'DELETE',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({status: 'sucess', msg: 'Deletado com sucesso!'})
+    })
+    const dados = await res.json()
+
+    console.log(dados)
+    retorno.innerHTML = dados.msg
 })
