ed197a285313b693a44b4a043aac587405902212
Mayck <103364944+MayckL2@users.noreply.github.com>
2023-12-29 19:22:55 -0300

    gerenciando cookie no back-end

--- back/app.js
+++ back/app.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const { stringify } = require('querystring')
+// const { stringify } = require('querystring')
 
 app.use(express.json())
 
 // CORS
 app.use((req, res, next) => {
     console.log('Cors funcionando!')
-    res.header('Access-Control-Allow-Origin', '*')
-    app.use(cors())
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5500')
+    // configuração de compartilhamento de credenciais
+    res.header("Access-Control-Allow-Credentials", "true");
+    app.use(cors({
+        origin: 'http://localhost:5500',
+        credentials: true,
+        methods: 'GET, POST, PUT, DELETE'
+    }))
     next()
 })
 
@@ -20,8 +26,12 @@ app.get('/prod', (req, res) => {
 
 // REQUISIÇÃO POST
 app.post('/prod', (req, res) => {
+    const { authorization } = req.headers
+
     if(req){
-        res.json({status: 'sucesso', msg: 'Retorno tipo POST!'})
+        // enviando cookie para o front-end com as configurações necessarias
+        res.cookie('token', authorization, {maxAge: 30000, httpOnly: true, secure: true, sameSite: 'none'})
+        res.json({status: 'sucesso', msg: 'Retorno tipo POST!', token: authorization})
         console.log(req.body)
     }else{
         res.json({status: 'false', msg: 'Erro na requisição!'})
@@ -54,5 +64,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(3000, () => {
-    console.log('Rodando com sucesso!')
+    console.log('Rodando com sucesso na porta 3000!')
 })
